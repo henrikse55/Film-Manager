f17cd1754fbadf6e6d47b6799e1a883ccf451799
henrikse55 <lasssenn@gmail.com>
2016-08-07 23:27:37 +0200

    added comment at line 21
    commented why the network thread is commented out

--- Client/Program.cs
+++ Client/Program.cs
@@ -18,6 +18,7 @@ namespace Client
         static void Main()
         {
             Thread.Sleep(500);
+            //Temporary Disabled Network comunication to be able to work on the client solo
             //new Thread(new ThreadStart(Network.Init)).Start();
 
             Application.EnableVisualStyles();
