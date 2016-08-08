4944d59d27bb6874431863f2afebfdb6a03b342f
henr054a <henr054a@HENRIK-PC>
2016-08-08 11:35:56 +0200

    Made a improved Editor for changing information on a movie more easely.

--- Client/Forms/Editor.cs
+++ Client/Forms/Editor.cs
@@ -12,9 +12,41 @@ namespace Client.Forms
 {
     public partial class Editor : Form
     {
-        public Editor()
+        DataGridView collection;
+
+        public Editor(DataGridView grid)
         {
             InitializeComponent();
+            collection = grid;
+        }
+
+        //TODO Lock Row for other users
+        private void Editor_Load(object sender, EventArgs e)
+        {
+            DataRow row = ((DataRowView)collection.SelectedRows[0].DataBoundItem).Row;
+            FilmTitleTextBox.Text = (String) row["Navn"];
+            GenreTextBox.Text = (String)row["Genre"];
+            LocationTextBox.Text = (String)row["Location"];
+            DescriptionBox.Text = (String)row["Description"];
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            var option = MessageBox.Show("Sure you wonna save these changes?", "Are You Sure", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            switch (option)
+            {
+                case DialogResult.Yes:
+                    DataRow row = ((DataRowView)collection.SelectedRows[0].DataBoundItem).Row;
+                    collection.SelectedRows[0].SetValues(row["Id"], FilmTitleTextBox.Text, GenreTextBox.Text, DescriptionBox.Text, LocationTextBox.Text);
+                    this.Close();
+                    break;
+            }
+
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
