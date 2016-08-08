4b4cbda8a5e85e108bbf7d0870c458a91772a9b3
henr054a <henr054a@HENRIK-PC>
2016-08-08 11:53:39 +0200

    Added a network message creater method
    Added a network message creator for making it easier for with making of
    network comunication

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,6 +3,9 @@ using System.Windows.Forms;
 using System.Threading;
 using Client.Network;
 using Client.Handler;
+using System.Text;
+using System.Collections.Generic;
+
 namespace Client
 {
     static class Program
@@ -23,6 +26,21 @@ namespace Client
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(clientform = new ClientForm());
+        }
+
+        /// <summary>
+        /// a simple methods for createing a network message to the server
+        /// </summary>
+        /// <param name="Command">What command is it you are sending</param>
+        /// <param name="args">exstra information the server might need</param>
+        public static string CreateNetworkMessage(String Command, params String[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Command);
+            List<String> list = new List<string>();
+            list.AddRange(args);
+            list.ForEach(x => sb.Append("-" + x));
+            return sb.ToString();
         }
     }
 }
