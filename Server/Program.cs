eaf6e24ab82d5c8e28521609117579af41af130c
Elias <Elias@ELIAS-PC>
2016-08-08 11:56:48 +0200

    DATABASE !!!!!! DRRRRRRRRRRRRRRRRRRRRROP THE TABELS!!!! WUB WUB WUB WUB!!!

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using Server.Network;
+using Server.Data;
 namespace Server
 {
     static class Program
     {
         public static ServerSide Network = new ServerSide();
         public static MessageHandler messageHandler = new MessageHandler();
+        public static DataHandler datahandler = new DataHandler();
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -19,6 +21,8 @@ namespace Server
         {
             new Thread(new ThreadStart(Network.Init)).Start();
 
+            datahandler.DeleteCommand(1);
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
