69189f62b86926b53395ad54aea83c8046214faf
henrikse55 <lasssenn@gmail.com>
2016-08-14 12:04:23 +0200

    Added UWP client [WIP]

--- Client/Network/Messages/SyncFilms.cs
+++ Client/Network/Messages/SyncFilms.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using Client.Interfaces;
 using Shared.Network;
 
@@ -19,12 +20,15 @@ namespace Client.Network.Messages
         {
             int Size = int.Parse(args[0]);
             Byte[] buffer = new byte[Size];
-            Program.Network.socket.BeginReceive(buffer,0,buffer.Length, 0, new AsyncCallback(asyncNet), Program.Network.socket);    
+            Program.Network.socket.BeginReceive(buffer,0,buffer.Length, 0, new AsyncCallback(asyncNet), Program.Network.socket);
+
+
         }
 
         private void asyncNet(IAsyncResult ar)
         {
-            
+            Socket socket = (Socket)ar.AsyncState;
+            int BytesRec = 
         }
     }
 }
